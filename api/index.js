4f5529e5ec0a2e29a4346e63bbdef583c8526005
joshuasam365 <joshuasam365@gmail.com>
2024-09-14 21:22:43 +0530

    passport integrated with cookies

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(cors({
-    origin:'http://localhost:3000',
+    origin:'https://joshuasblog.vercel.app',
     credentials:true
 
 }));
