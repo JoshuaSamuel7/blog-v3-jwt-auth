5c2629e95dfadbeb1a7b4b4031e7f1dd505d0915
joshuasam365 <joshuasam365@gmail.com>
2024-09-14 13:15:08 +0530

    passport integrated

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -2,7 +2,7 @@ const User=require('../models/user');
 const express=require('express');
 const router=express.Router();
 const authControllers=require('../controllers/Authcontrollers');
-
+const passport=require("../config/passport")
 router.post("/login",authControllers.postLogin);
 router.post("/register",authControllers.postRegister);
 router.get("/current_user",passport.authenticate('jwt', { session: false }),authControllers.currentUser);
